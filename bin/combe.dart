57f379a8b6f1e0f23e617b8c8d372a753cb5635b
Sergio <sagdpr03@gmail.com>
2024-12-06 02:10:29 -0500

    join command added

--- bin/combe.dart
+++ bin/combe.dart
@@ -1,5 +1,6 @@
-import 'package:combe/combe.dart' as combe;
+import 'package:combe/combe.dart';
 import 'package:nyxx/nyxx.dart';
+import 'package:nyxx_lavalink/nyxx_lavalink.dart';
 import 'package:subsonic_api/subsonic_api.dart';
 import 'package:toml/toml.dart';
 
@@ -7,13 +8,37 @@ final String configFile = 'config.toml';
 
 Future<void> main() async {
   final toml = await TomlDocument.load(configFile);
-  final config = combe.Config.fromToml(toml.toMap());
+  final config = Config.fromToml(toml.toMap());
 
-  final client = await Nyxx.connectGateway(
-      config.discordToken, GatewayIntents.allUnprivileged);
-
-  final user = await client.users.fetchCurrentUser();
+  final client =
+      await Nyxx.connectGateway(config.discordToken, GatewayIntents.all);
 
   final subsonic = SubSonicClient(config.subsonicUrl, config.subsonicUser,
       config.subsonicToken, config.subsonicSalt, 'ceombe', '1.16.1');
+
+  addCommand(client, config, 'join', (event) async {
+    // get the voice channel the user is in
+    final voiceChannel = client
+        .guilds[event.guildId!].voiceStates[event.message.author.id]!.channel;
+
+    // join the voice channel
+    client.updateVoiceState(
+        event.guildId!,
+        GatewayVoiceStateBuilder(
+            channelId: voiceChannel!.id, isMuted: false, isDeafened: true));
+  });
+
+  client.onEvent.listen((event) {
+    if (event is VoiceServerUpdateEvent) {
+      final endpoint = event.endpoint;
+      final token = event.token;
+      final guildId = event.guildId;
+
+      // connect to the websocet and start streaming audio
+    }
+
+    if (event is VoiceStateUpdateEvent) {
+      // check if joined the voice channel successfully
+    }
+  });
 }
--- lib/combe.dart
+++ lib/combe.dart
@@ -2,19 +2,32 @@ import 'package:nyxx/nyxx.dart';
 import 'package:subsonic_api/subsonic_api.dart';
 import 'package:toml/toml.dart';
 
+void addCommand(NyxxGateway client, Config config, String command,
+    Future<void> Function(MessageCreateEvent) callback) {
+  client.onMessageCreate.listen((event) async {
+    if (event.message.content == '${config.commandPrefix}$command') {
+      await callback(event);
+    }
+  });
+}
+
+class Player {}
+
 class Config {
   String discordToken;
+  String commandPrefix;
   String subsonicUrl;
   String subsonicUser;
   String subsonicSalt;
   String subsonicToken;
 
-  Config(this.discordToken, this.subsonicUrl, this.subsonicUser,
-      this.subsonicSalt, this.subsonicToken);
+  Config(this.discordToken, this.commandPrefix, this.subsonicUrl,
+      this.subsonicUser, this.subsonicSalt, this.subsonicToken);
 
   factory Config.fromToml(Map<String, dynamic> toml) {
-    Config config = Config('', '', '', '', '');
-    config.discordToken = toml['general']['token'];
+    Config config = Config('', '', '', '', '', '');
+    config.discordToken = toml['discord']['token'];
+    config.commandPrefix = toml['discord']['prefix'];
     config.subsonicUrl = toml['server']['url'];
     config.subsonicUser = toml['server']['username'];
     config.subsonicSalt = createSalt();
@@ -25,6 +38,6 @@ class Config {
 
   @override
   String toString() {
-    return 'Config(token: $discordToken, subsonicUrl: $subsonicUrl, subsonicUser: $subsonicUser, subsonicSalt: $subsonicSalt, subsonicToken: $subsonicToken)';
+    return 'Config(token: $discordToken, prefix: $commandPrefix, subsonicUrl: $subsonicUrl, subsonicUser: $subsonicUser, subsonicSalt: $subsonicSalt, subsonicToken: $subsonicToken)';
   }
 }
