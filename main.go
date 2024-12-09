763357232cc664fe91a80f840e19204e3e2be983
Sergio <sagdpr03@gmail.com>
2024-12-09 12:30:32 -0500

    search

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
-	// "github.com/delucks/go-subsonic"
-	// "net/http"
+	"github.com/delucks/go-subsonic"
 	"github.com/pelletier/go-toml/v2"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -27,6 +27,7 @@ type Config struct {
 }
 
 var config Config
+var subsonicClient subsonic.Client
 
 func commandHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
@@ -37,32 +38,103 @@ func commandHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	println("Command: ", m.Content)
+
 	if m.Content == config.Discord.Prefix+"ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	} else if strings.HasPrefix(m.Content, config.Discord.Prefix+"search") {
+		fmt.Println("Search command")
+
+		query := strings.TrimPrefix(m.Content, config.Discord.Prefix+"search")
+
+		result, err := subsonicClient.Search3(query, map[string]string{})
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+
+		if len(result.Song) == 0 {
+			s.ChannelMessageSend(m.ChannelID, "No results found.")
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, "Results: ")
+
+		var str strings.Builder
+
+		str.WriteString("Artist | Title\n")
+		str.WriteString("```")
+		for _, song := range result.Song {
+			str.WriteString(fmt.Sprintf("%s - %s\n", song.Artist, song.Title))
+		}
+		str.WriteString("```")
+
+		s.ChannelMessageSend(m.ChannelID, str.String())
+
 	}
 }
 
 func main() {
 	config.loadToml()
 
-	discord, err := discordgo.New("Bot " + config.Discord.Token)
+	discord, err := connectToDiscord()
 
 	if err != nil {
-		fmt.Println("Error creating Discord session: ", err)
+		fmt.Println("Error: ", err)
 		return
 	}
 
+	subsonicClient, err = connectToSubsonic()
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	defer discord.Close()
+
 	discord.AddHandler(commandHandler)
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	<-make(chan struct{})
+}
+
+func connectToSubsonic() (subsonic.Client, error) {
+	httpClient := &http.Client{}
+
+	subsonicClient := subsonic.Client{
+		Client:       httpClient,
+		BaseUrl:      config.Server.Url,
+		User:         config.Server.Username,
+		ClientName:   "Ceombe",
+		PasswordAuth: true,
+	}
+
+	err := subsonicClient.Authenticate(config.Server.Password)
+
+	if err != nil {
+		return subsonic.Client{}, err
+	}
+
+	return subsonicClient, nil
+}
+
+func connectToDiscord() (*discordgo.Session, error) {
+
+	discord, err := discordgo.New("Bot " + config.Discord.Token)
+
+	if err != nil {
+		return nil, err
+	}
 
 	err = discord.Open()
 
 	if err != nil {
-		fmt.Println("Error opening connection: ", err)
-		return
+		return nil, err
 	}
 
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
-	<-make(chan struct{})
+	return discord, nil
 }
 
 func (c *Config) loadToml() {
