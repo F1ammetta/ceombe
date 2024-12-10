cd8743f1dcffa0241f1bc9076613abaae2115dd9
Sergio <sagdpr03@gmail.com>
2024-12-10 00:03:17 -0500

    reduced the chaos

---
+++ handlers.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	// "net/http"
+	// "os"
+	"os/exec"
+	"strings"
+
+	// "ceombe/go-subsonic"
+
+	"github.com/bwmarrin/discordgo"
+	// "github.com/pelletier/go-toml/v2"
+	"layeh.com/gopus"
+)
+
+func handleJoinCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	user := m.Author
+
+	voiceState, err := s.State.VoiceState(m.GuildID, user.ID)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	if voiceState == nil {
+		s.ChannelMessageSend(m.ChannelID, "You need to be in a voice channel to use this command.")
+		return
+	}
+
+	_, err = s.ChannelVoiceJoin(m.GuildID, voiceState.ChannelID, false, true)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, "Joined voice channel.")
+}
+
+func handleSearchCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fmt.Println("Search command")
+
+	query := strings.TrimPrefix(m.Content, config.Discord.Prefix+"search")
+
+	result, err := subsonicClient.Search3(query, map[string]string{})
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	if len(result.Song) == 0 {
+		s.ChannelMessageSend(m.ChannelID, "No results found.")
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, "Results: ")
+
+	var str strings.Builder
+
+	str.WriteString("```")
+	str.WriteString("Artist - Title\n")
+	for _, song := range result.Song {
+		str.WriteString(fmt.Sprintf("%s - %s\n", song.Artist, song.Title))
+	}
+	str.WriteString("```")
+
+	s.ChannelMessageSend(m.ChannelID, str.String())
+}
+
+func handlePlayCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	user := m.Author
+
+	voiceState, err := s.State.VoiceState(m.GuildID, user.ID)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	if voiceState == nil {
+		s.ChannelMessageSend(m.ChannelID, "You need to be in a voice channel to use this command.")
+		return
+	}
+
+	chann, err := s.ChannelVoiceJoin(m.GuildID, voiceState.ChannelID, false, true)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	// s.ChannelMessageSend(m.ChannelID, "Joined voice channel.")
+
+	query := strings.TrimPrefix(m.Content, config.Discord.Prefix+"play")
+
+	result, err := subsonicClient.Search3(query, map[string]string{})
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	if len(result.Song) == 0 {
+		s.ChannelMessageSend(m.ChannelID, "No results found.")
+		return
+	}
+
+	song := result.Song[0]
+
+	streamUrl, err := subsonicClient.GetStreamUrl(song.ID, map[string]string{"format": "opus", "maxBitRate": "128"})
+
+	// stream, err := subsonicClient.Stream(song.ID, map[string]string{"format": "opus", "maxBitRate": "128"})
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	chann.Speaking(true)
+	if player.Playing == "" {
+		player.Playing = streamUrl
+		player.Query = query
+	} else {
+		player.Queue = append(player.Queue, query)
+		s.ChannelMessageSend(m.ChannelID, "Added to the queue.")
+		println("Queue: ", player.Queue)
+		return
+	}
+	bytesPerSecond := int64(48000 * 2 * 2) // SampleRate * Channels * BytesPerSample
+	seekTime := float64(player.Position) / float64(bytesPerSecond)
+	cmd := exec.Command("ffmpeg", "-re",
+		"-ss", fmt.Sprintf("%.2f", seekTime),
+		"-i", player.Playing,
+		"-f", "s16le",
+		"-ac", "2",
+		"-ar", "48000",
+		"-application", "lowdelay",
+		"pipe:1")
+
+	ffmpegOut, err := cmd.StdoutPipe()
+	ffmpegErr, err := cmd.StderrPipe()
+
+	go func() {
+		scanner := bufio.NewScanner(ffmpegErr)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	go func() {
+		err := cmd.Start()
+
+		encoder, err := gopus.NewEncoder(48000, 2, gopus.Audio)
+		frameSize := 960
+		maxBytes := 960 * 2 * 2
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+			if err.Error() != "EOF" {
+				return
+			}
+		}
+
+		reader := bufio.NewReader(ffmpegOut)
+		var position int64 = player.Position
+
+		for {
+			if player.Paused || player.Skip {
+				break
+			}
+			buffer := make([]byte, 4096)
+
+			n, err := reader.Read(buffer)
+
+			if err != nil {
+				fmt.Println("Error: ", err)
+				if err.Error() != "EOF" {
+					break
+				}
+			}
+
+			if n == 0 {
+				break
+			}
+
+			encbuf := make([]int16, frameSize*2)
+
+			for i := 0; i < maxBytes/2; i++ {
+				encbuf[i] = int16(buffer[i*2]) | int16(buffer[i*2+1])<<8
+			}
+
+			opus, err := encoder.Encode(encbuf, frameSize, maxBytes)
+
+			if err != nil {
+				fmt.Println("Error: ", err)
+				if err.Error() != "EOF" {
+					break
+				}
+			}
+
+			if chann.Ready == false || chann.OpusSend == nil {
+
+				return
+			}
+
+			chann.OpusSend <- opus
+			position += int64(n)
+		}
+		player.Playing = ""
+
+		player.Skip = false
+		println("Song finished.", player.Skip)
+		if player.Paused {
+			player.Position = position
+		} else if len(player.Queue) > 0 {
+			player.Query = ""
+			s.ChannelMessageSend(m.ChannelID, "Playing next song.")
+			println("Queue: ", player.Queue)
+			var newS *discordgo.MessageCreate = m
+			newS.Content = config.Discord.Prefix + "play" + player.Queue[0]
+			player.Queue = player.Queue[1:]
+			player.Position = 0
+			commandHandler(s, newS)
+
+		} else if player.Loop {
+			player.Query = ""
+			var newS *discordgo.MessageCreate = m
+			newS.Content = config.Discord.Prefix + "play" + query
+			player.Position = 0
+			s.ChannelMessageSend(m.ChannelID, "Looping song.")
+			commandHandler(s, newS)
+
+		} else {
+			player.Query = ""
+			player.Position = 0
+		}
+
+	}()
+
+}
--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
 	"fmt"
 	"net/http"
 	"os"
-	"os/exec"
+	// "os/exec"
 	"strings"
 
 	"ceombe/go-subsonic"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/pelletier/go-toml/v2"
-	"layeh.com/gopus"
+	// "layeh.com/gopus"
 )
 
 type Server struct {
@@ -56,288 +56,47 @@ func commandHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	println("Command: ", m.Content)
 
 	if m.Content == config.Discord.Prefix+"ping" {
+
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
+
 	} else if strings.HasPrefix(m.Content, config.Discord.Prefix+"search") {
-		fmt.Println("Search command")
 
-		query := strings.TrimPrefix(m.Content, config.Discord.Prefix+"search")
+		handleSearchCommand(s, m)
 
-		result, err := subsonicClient.Search3(query, map[string]string{})
+	} else if strings.HasPrefix(m.Content, config.Discord.Prefix+"play") || strings.HasPrefix(m.Content, config.Discord.Prefix+"p") {
 
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		if len(result.Song) == 0 {
-			s.ChannelMessageSend(m.ChannelID, "No results found.")
-			return
-		}
-
-		s.ChannelMessageSend(m.ChannelID, "Results: ")
-
-		var str strings.Builder
-
-		str.WriteString("```")
-		str.WriteString("Artist - Title\n")
-		for _, song := range result.Song {
-			str.WriteString(fmt.Sprintf("%s - %s\n", song.Artist, song.Title))
-		}
-		str.WriteString("```")
-
-		s.ChannelMessageSend(m.ChannelID, str.String())
-
-	} else if strings.HasPrefix(m.Content, config.Discord.Prefix+"play") {
-		user := m.Author
-
-		voiceState, err := s.State.VoiceState(m.GuildID, user.ID)
-
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		if voiceState == nil {
-			s.ChannelMessageSend(m.ChannelID, "You need to be in a voice channel to use this command.")
-			return
-		}
-
-		chann, err := s.ChannelVoiceJoin(m.GuildID, voiceState.ChannelID, false, true)
-
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		s.ChannelMessageSend(m.ChannelID, "Joined voice channel.")
-
-		query := strings.TrimPrefix(m.Content, config.Discord.Prefix+"play")
-
-		result, err := subsonicClient.Search3(query, map[string]string{})
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		if len(result.Song) == 0 {
-			s.ChannelMessageSend(m.ChannelID, "No results found.")
-			return
-		}
-
-		song := result.Song[0]
-
-		streamUrl, err := subsonicClient.GetStreamUrl(song.ID, map[string]string{"format": "opus", "maxBitRate": "128"})
-
-		// stream, err := subsonicClient.Stream(song.ID, map[string]string{"format": "opus", "maxBitRate": "128"})
-
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		chann.Speaking(true)
-		if player.Playing == "" {
-			player.Playing = streamUrl
-			player.Query = query
-		} else {
-			player.Queue = append(player.Queue, query)
-			s.ChannelMessageSend(m.ChannelID, "Added to the queue.")
-			println("Queue: ", player.Queue)
-			return
-		}
-		bytesPerSecond := int64(48000 * 2 * 2) // SampleRate * Channels * BytesPerSample
-		seekTime := float64(player.Position) / float64(bytesPerSecond)
-		cmd := exec.Command("ffmpeg", "-re",
-			"-ss", fmt.Sprintf("%.2f", seekTime),
-			"-i", player.Playing,
-			"-f", "s16le",
-			"-ac", "2",
-			"-ar", "48000",
-			"-b:a", "128k",
-			"-application", "lowdelay",
-			"pipe:1")
-
-		// ffmpegIn, err := cmd.StdinPipe()
-		ffmpegOut, err := cmd.StdoutPipe()
-		ffmpegErr, err := cmd.StderrPipe()
-
-		go func() {
-			scanner := bufio.NewScanner(ffmpegErr)
-			for scanner.Scan() {
-				fmt.Println(scanner.Text())
-			}
-		}()
-
-		go func() {
-			err := cmd.Start()
-
-			encoder, err := gopus.NewEncoder(48000, 2, gopus.Audio)
-			frameSize := 960
-			maxBytes := 960 * 2 * 2
-
-			if err != nil {
-				fmt.Println("Error: ", err)
-				if err.Error() != "EOF" {
-					return
-				}
-			}
-
-			reader := bufio.NewReader(ffmpegOut)
-			var position int64 = player.Position
-
-			for {
-				if player.Paused || player.Skip {
-					break
-				}
-				buffer := make([]byte, 4096)
-
-				n, err := reader.Read(buffer)
-
-				if err != nil {
-					fmt.Println("Error: ", err)
-					if err.Error() != "EOF" {
-						break
-					}
-				}
-
-				if n == 0 {
-					break
-				}
-
-				encbuf := make([]int16, frameSize*2)
-
-				for i := 0; i < maxBytes/2; i++ {
-					encbuf[i] = int16(buffer[i*2]) | int16(buffer[i*2+1])<<8
-				}
-
-				opus, err := encoder.Encode(encbuf, frameSize, maxBytes)
-
-				if err != nil {
-					fmt.Println("Error: ", err)
-					if err.Error() != "EOF" {
-						break
-					}
-				}
-
-				if chann.Ready == false || chann.OpusSend == nil {
-
-					return
-				}
-
-				//println("Sending opus packet of size: ", len(opus))
-
-				chann.OpusSend <- opus
-				position += int64(n)
-			}
-			player.Playing = ""
-
-			player.Skip = false
-			println("Song finished.", player.Skip)
-			if player.Paused {
-				player.Position = position
-			} else if len(player.Queue) > 0 {
-				player.Query = ""
-				s.ChannelMessageSend(m.ChannelID, "Playing next song.")
-				println("Queue: ", player.Queue)
-				var newS *discordgo.MessageCreate = m
-				newS.Content = config.Discord.Prefix + "play" + player.Queue[0]
-				//println("newM: ", newS.Content)
-				player.Queue = player.Queue[1:]
-				player.Position = 0
-				commandHandler(s, newS)
-
-			} else if player.Loop {
-				player.Query = ""
-				var newS *discordgo.MessageCreate = m
-				newS.Content = config.Discord.Prefix + "play" + query
-				player.Position = 0
-				s.ChannelMessageSend(m.ChannelID, "Looping song.")
-				commandHandler(s, newS)
-
-			} else {
-				player.Query = ""
-				player.Position = 0
-			}
-
-		}()
-
-		// for {
-		// 	streambuffer := make([]byte, 4096)
-		//
-		// 	n, err := stream.Read(streambuffer)
-		//
-		// 	if err != nil {
-		// 		fmt.Println("get stream Error: ", err)
-		// 		break
-		// 	}
-		//
-		// 	if n == 0 {
-		// 		break
-		// 	}
-		//
-		// 	ffmpegIn.Write(streambuffer)
-		// }
-
-		// for {
-		// 	buffer := make([]byte, 320)
-		// 	n, err := stream.Read(buffer)
-		//
-		// 	if err != nil {
-		// 		fmt.Println("Error: ", err)
-		// 		break
-		// 	}
-		//
-		// 	if n == 0 {
-		// 		break
-		// 	}
-		//
-		// 	// fmt.Println("buffer: ", buffer)
-		//
-		// 	chann.OpusSend <- buffer
-		//
-		// }
+		handlePlayCommand(s, m)
 
 	} else if m.Content == config.Discord.Prefix+"join" {
-		user := m.Author
 
-		voiceState, err := s.State.VoiceState(m.GuildID, user.ID)
+		handleJoinCommand(s, m)
 
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
+	} else if m.Content == config.Discord.Prefix+"skip" || m.Content == config.Discord.Prefix+"s" {
 
-		if voiceState == nil {
-			s.ChannelMessageSend(m.ChannelID, "You need to be in a voice channel to use this command.")
-			return
-		}
-
-		_, err = s.ChannelVoiceJoin(m.GuildID, voiceState.ChannelID, false, true)
-
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		s.ChannelMessageSend(m.ChannelID, "Joined voice channel.")
-
-	} else if m.Content == config.Discord.Prefix+"skip" {
 		player.Skip = true
 		s.ChannelMessageSend(m.ChannelID, "Skipping song.")
+
 	} else if m.Content == config.Discord.Prefix+"pause" {
+
 		player.Paused = true
 		s.ChannelMessageSend(m.ChannelID, "Paused.")
+
 	} else if m.Content == config.Discord.Prefix+"resume" {
+
 		player.Paused = false
 		s.ChannelMessageSend(m.ChannelID, "Resumed.")
 		var newS *discordgo.MessageCreate = m
 		newS.Content = config.Discord.Prefix + "play " + player.Query //for now at least
 		commandHandler(s, newS)
+
 	} else if m.Content == config.Discord.Prefix+"loop" {
+
 		player.Loop = !player.Loop
-		if player.Loop {
+
+		switch player.Loop {
+		case true:
 			s.ChannelMessageSend(m.ChannelID, "Looping enabled.")
-		} else {
+		case false:
 			s.ChannelMessageSend(m.ChannelID, "Looping disabled.")
 		}
 	}
