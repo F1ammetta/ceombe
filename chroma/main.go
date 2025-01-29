95a5ac371319f09231ca8afdee0ad3cfd428cf79
Sergio <sagdpr03@gmail.com>
2025-01-29 02:27:30 -0500

    fixed integration

--- chroma/main.go
+++ chroma/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/bogem/id3v2/v2"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+
+	"github.com/bogem/id3v2/v2"
 )
 
 func GetFingerprint(filePath string) (string, int, error) {
-	execPath, err := exec.LookPath("fpcalc")
+	execPath, err := exec.LookPath("/usr/bin/fpcalc")
 	if err != nil {
 		return "", -1, err
 	}
@@ -47,6 +48,7 @@ func GetCoverImage(albumId string) []byte {
 	// fetch cover image
 	res, err := http.Get("https://coverartarchive.org/release-group/" + albumId)
 	if err != nil {
+		fmt.Println("error making request")
 		fmt.Println(err)
 		return nil
 	}
@@ -55,6 +57,7 @@ func GetCoverImage(albumId string) []byte {
 	// print body
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
+		fmt.Println("unable to read response data")
 		fmt.Println(err)
 		return nil
 	}
@@ -64,6 +67,7 @@ func GetCoverImage(albumId string) []byte {
 	err = json.Unmarshal(resBody, &data)
 
 	if err != nil {
+		fmt.Println("unable to unmarshal response")
 		fmt.Println(err)
 		return nil
 	}
@@ -78,6 +82,7 @@ func GetCoverImage(albumId string) []byte {
 	// get image data and return
 	res, err = http.Get(image_url)
 	if err != nil {
+		fmt.Println("unable to retreive image")
 		fmt.Println(err)
 		return nil
 	}
@@ -87,6 +92,7 @@ func GetCoverImage(albumId string) []byte {
 	imageData, err := io.ReadAll(res.Body)
 
 	if err != nil {
+		fmt.Println("unable to read image data")
 		fmt.Println(err)
 		return nil
 	}
@@ -97,17 +103,24 @@ func GetCoverImage(albumId string) []byte {
 func main() {
 	args := os.Args
 
-	if len(args) < 2 {
-		fmt.Println("Please provide a file path")
-		return
-	}
+	// fmt.Printf("%v\n", args)
+
+	fmt.Println(len(args))
+
+	// if len(args) < 2 {
+	// 	fmt.Println("Please provide a file path")
+	// 	return
+	// }
 
 	filePath := args[1]
 
+	fmt.Println("getting fringerprint")
 	// call fpcalc to get fingerprint
 	fingerprint, duration, err := GetFingerprint(filePath)
+	fmt.Println("fringerprint success")
 
 	if err != nil {
+		fmt.Println("getting fingered")
 		fmt.Println(err)
 		return
 	}
@@ -119,7 +132,9 @@ func main() {
 		Metadata:    "recordings+releasegroups+compress",
 	}
 
+	fmt.Println("making request to acoustid")
 	response := request.Do()
+	fmt.Println("request success")
 
 	if len(response.Results) == 0 {
 		fmt.Println("No results found")
@@ -135,16 +150,21 @@ func main() {
 	print("Artist: " + Artist + "\n")
 	print("Album: " + Album + "\n")
 
+	fmt.Println("getting image")
 	image := GetCoverImage(AlbumId)
+	fmt.Println("got image")
 
 	tag, err := id3v2.Open(filePath, id3v2.Options{Parse: true})
 
 	if err != nil {
+		fmt.Println("unable to open file")
 		fmt.Println(err)
 		return
 	}
 
 	defer tag.Close()
+
+	fmt.Println("setting tags")
 
 	tag.SetTitle(Title)
 	tag.SetArtist(Artist)
@@ -159,10 +179,14 @@ func main() {
 			Picture:     image,
 		}
 
+		fmt.Println("adding pic")
+
 		tag.AddAttachedPicture(pic)
 	}
 
+	fmt.Println("saving")
 	if err = tag.Save(); err != nil {
+		fmt.Println("unable to write to file")
 		fmt.Println(err)
 		return
 	}
